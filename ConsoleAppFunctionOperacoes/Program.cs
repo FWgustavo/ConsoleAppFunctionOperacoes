085a406933227f90af56fea00ba4a0bc7c1980d0
Usuario <Usuario@CPD02-M01>
2024-05-28 10:46:44 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppFunctionOperacoes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFunctionOperacoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro número: ");
+            double n1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a operação(+,-,/,*): ");
+            string op = Console.ReadLine();
+
+
+            ope(n1 , n2, op);
+
+            Console.ReadKey();
+        }
+
+        static void ope(double n1, double n2, string op) 
+        {
+            double resultado;
+            switch (op)
+            {
+                case "+": 
+                    resultado = n1 + n2;
+                    Console.WriteLine("O resultado da sua soma é: "+resultado);
+                break;
+
+                case "-":
+                    resultado = n1 - n2;
+                    Console.WriteLine("O resultado da subtração é: " + resultado);
+                break;
+                case "*":
+                    resultado = n1 * n2;
+                    Console.WriteLine("O resultado da multiplicação é: " + resultado);
+                break;
+                case "/":
+                    resultado = n1 / n2;
+                    Console.WriteLine("O resultado da divisão é: " + resultado);
+                break;
+                default: 
+                    Console.WriteLine("Operação inserida inválida ");
+                break;
+            }
+        }
+    }
+}
